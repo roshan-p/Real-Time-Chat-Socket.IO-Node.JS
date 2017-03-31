8ea2e6864aaa90cfca429909716b2feef102df12
roshan-p <roshan007191@gmail.com>
2017-03-31 22:39:38 +0700

    ..

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
 var mongo = require('mongodb').MongoClient,
     client = require('socket.io').listen(8080).sockets;
 
-mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
-  if(err) {throw err;}
-  client.on('connection', function(socket){
-    console.log('Someone connected!');
-    //messages is table or collection name not a database
-    var col = db.collection('messages');
-        socket.on('input', function(data){
-           // console.log(data)
-           var name = data.name,
-               message = data.message;
-                whitespacePattern = /^\s*$/;
+mongo.connect('mongodb://127.0.0.1/chat', function (err, db) {
+    if (err) { throw err; }
+    client.on('connection', function (socket) {
+        console.log('Someone connected!');
+        //messages is table or collection name not a database
+        var col = db.collection('messages'),
+            sendStatus = function (s) {
+                socket.emit('status', s);
+            };
+        //Emit all messages
+        col.find().limit(50).sort({ _id: 1 }).toArray(function (err, res) {
+            if (err) { sendStatus('Error fetching messages.'); }
 
-      if(whitespacePattern.test(name) || whitespacePattern.test(message)){
-          //Cannot insert empty string.
-        console.log('Invalid!');
-      } else {
-            col.insert({name: name, message: message},function(){
-                console.log('Inserted')
-            });
+            socket.emit('output', res);
+        });
 
-      }
+        //wait for input
+        socket.on('input', function (data) {
+            // console.log(data)
+            var name = data.name,
+                message = data.message;
+            whitespacePattern = /^\s*$/;
+
+            if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+                //Cannot insert empty string.
+                sendStatus('Name and Message is required!');
+
+            } else {
+                col.insert({ name: name, message: message }, function () {
+                    //emit later message to all clients
+                    client.emit('output',[data]);
+                    sendStatus({
+                        message: "Message sent",
+                        clear: true
+                    });
+                });
+
+            }
 
 
         });
